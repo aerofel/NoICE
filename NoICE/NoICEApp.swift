1f2ba7d4021b2c1ff5ca3f3c93191982f002fc93
Philippe Le Gall <looping@looping.org>
2026-01-16 21:36:53 +0100

    Initial Commit

---
+++ NoICE/NoICEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoICEApp.swift
+//  NoICE
+//
+//  Created by Philippe LE GALL on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct NoICEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
